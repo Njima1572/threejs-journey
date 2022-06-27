007504553fa8bc30b2de8f16cad5d8efa461e757
Njima1572 <kochi.nakajima@gmail.com>
2022-06-27 14:20:42 -0400

    some more addition

--- src/App.tsx
+++ src/App.tsx
@@ -61,10 +61,11 @@ function App() {
       const tick = () => {
         // Time
         const elapsedTime = clock.getElapsedTime()
-
+        // DONT use getDelta
         group.rotation.x = elapsedTime * Math.PI / 4 
-        group.position.x = Math.cos(elapsedTime)
-        group.position.y = Math.sin(elapsedTime)
+        camera.position.x = Math.cos(elapsedTime)
+        camera.position.y = Math.sin(elapsedTime)
+        camera.lookAt(group.position)
         renderer.render(scene, camera)
         window.requestAnimationFrame(tick)
       }
