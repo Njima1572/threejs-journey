e7652fa8bb0e4782b5459f4f1bd2ee020835110b
Njima1572 <kochi.nakajima@gmail.com>
2022-06-27 13:38:25 -0400

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
+import { useEffect } from 'react'
+import * as THREE from 'three'
 
 function App() {
-  return (
-    <div>
 
-    </div>
+  useEffect(() => {
+    const canvas = document.querySelector('canvas.webgl')
+    const scene = new THREE.Scene()
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    const mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
+
+    const sizes = {
+      width: 800,
+      height: 600
+    }
+
+    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+    camera.position.z = 3
+    scene.add(camera)
+
+    if (canvas) {
+      const renderer = new THREE.WebGLRenderer({
+        canvas: canvas
+      })
+      renderer.setSize(sizes.width, sizes.height)
+      renderer.render(scene, camera)
+    }
+
+  }, [])
+
+  return (
+    <canvas className="webgl">
+    </canvas>
   )
 }
 
