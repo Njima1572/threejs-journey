72f0499e48b73d85fcd71c58c16a294e3622233a
Njima1572 <kochi.nakajima@gmail.com>
2022-06-27 13:57:15 -0400

    Rotation done

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,26 @@ function App() {
     const geometry = new THREE.BoxGeometry(1, 1, 1)
     const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
     const mesh = new THREE.Mesh(geometry, material)
-    mesh.position.x = 1 // Should move right for a unit
-    mesh.position.y = -1 // Should move down for a unit
+    // mesh.position.x = 1 // Should move right for a unit
+    // mesh.position.y = -1 // Should move down for a unit
     // mesh.position is type of Vector3 : https://threejs.org/docs/#api/en/math/Vector3
 
     // alternatively
-    mesh.position.set(1, -1, 1)
+    mesh.position.set(1, 0, 1)
     scene.add(mesh)
+
+    // Changes after adding the mesh to scene, also applies as long as it is before render
+    mesh.scale.x = 2
+
+    // TO rotate, we can use either rotation or quaternion
+    // Uses 2pi = 360
+    // The order matters
+    mesh.rotation.reorder("XYZ") // Defaults to XYZ and do it before changing rotation
+    mesh.rotation.x = Math.PI / 2 // should be 60 degrees by x axis
+    mesh.rotation.y = Math.PI / 4 // should be 60 degrees by x axis
+    mesh.rotation.z = Math.PI / 4 // should be 60 degrees by x axis
+
+    // OR use quaternion instead to avoid gimbal lock
 
     const sizes = {
       width: 800,
@@ -35,7 +48,9 @@ function App() {
     // distanceTo is Vector3 to Vector3
     console.log(mesh.position.distanceTo(camera.position)) // non-trivial, useable line for some use
 
-    mesh.position.normalize() // Shrink / expand until the length is 1
+    // console.log(mesh.position.length())
+    // mesh.position.normalize() // Shrink / expand until the length is 1
+    // console.log(mesh.position.length()) // Should be 1
 
     if (canvas) {
       const renderer = new THREE.WebGLRenderer({
